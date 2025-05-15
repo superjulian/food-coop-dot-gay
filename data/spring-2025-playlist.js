77d2910cbb4484a6b3a0fe8cca95e6cc68f47b62
Julian Meltzer <julian.meltzer@nytimes.com>
2025-05-14 21:15:43 -0400

    adding resources.

---
+++ data/spring-2025-playlist.js
@@ -0,0 +1,66 @@
+const playlistData = [
+    {   
+        dj: "Mr. Anderson",
+        songs:
+            [
+                { title: "The Catastrophe", artist: "Car Seat Headrest" },
+                { title: "How Bad Do U Want Me", artist: "Lady Gaga" },
+                { title: "Somebody That I Used to Know", artist: "Gotye" },
+                { title: "Happier Than Ever", artist: "Billie Eilish" }
+            ]
+    },
+    { 
+        dj: "frederick",
+        songs:
+            [
+                { title: "Floating On Salt Water", artist: "A Sagittariun" },
+                { title: "Body Move", artist: "Dizzy Fae" },
+                { title: "CAN'T TELL ME NOTHING", artist: "Kalisway" },
+                { title: "Piano Concerto 1 in E minor, Op 11", artist: "Frédéric Chopin" },
+            ]
+    },
+    { 
+        dj: "sue me",
+        songs:
+            [
+                { title: "Heat", artist: "Toni Braxton" },
+                { title: "Move 4 Fire", artist: "Trancemaster" },
+                { title: "Marry Me", artist: "DJ Call Me" },
+                { title: "All Night Long", artist: "Faith Evans ft P Diddy unfortunately" },
+            ]
+    },
+]
+const colors = [
+    {
+        background_color: "#ff0000",
+        text_color: "#00ff00",
+    },
+    {
+        background_color: "#00ffff",
+        text_color: "#ffff00",
+    },
+    {
+        background_color: "#ff00ff",
+        text_color: "#00ffff",
+    },
+    {
+        background_color: "#ffffff",
+        text_color: "#000000",
+    },
+    {
+        background_color: "#808080",
+        text_color: "#800000",
+    },
+    {
+        background_color: "#008000",
+        text_color: "#000080",
+    },
+    {
+        background_color: "#808000",
+        text_color: "#800080",
+    },
+    {
+        background_color: "#008080",
+        text_color: "#ffffff",
+    }
+]
